ae900a96737be2cd1021fb9f86c97cdf2cdd13df
orrmb <orr4353@gmail.com>
2024-08-26 20:32:20 +0300

    test the action

--- Guest_management/tests_porject/test-add-users.py
+++ Guest_management/tests_porject/test-add-users.py
@@ -16,6 +16,7 @@ for i in range(10):
                 "side": side,
                 "relationship": relationship}
 
+
         response = requests.post('http://127.0.0.1:5000/submit', headers=headers, data=data)
         print(response)
 
