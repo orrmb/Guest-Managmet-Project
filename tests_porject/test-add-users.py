53c81f9da52e810913e64de7272cc6595758b423
orrmb <orr4353@gmail.com>
2024-08-29 20:34:20 +0300

    UW

--- tests_porject/test-add-users.py
+++ tests_porject/test-add-users.py
@@ -17,8 +17,8 @@ for i in range(10):
           "relationship": relationship}
 
   response = requests.post('http://127.0.0.1:8080/submit', headers=headers, data=data)
-  if response != '<Response [200]>':
-        exit(code=500)
+  if response.status_code != 200:
+    exit(code=500)
 
       
 
